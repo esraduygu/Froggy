b52c0c3b25b59144823a51518a7f1f4d830e314b
Esra Duygu <benesrakaya@gmail.com>
2024-03-29 15:54:03 +0300

    Added playerState and working on bugs.

--- Assets/Scripts/Player/PlayerCollisionHandler.cs
+++ Assets/Scripts/Player/PlayerCollisionHandler.cs
@@ -5,23 +5,42 @@ namespace Player
     public class PlayerCollisionHandler : MonoBehaviour
     {
         [SerializeField] private PlayerMovementController playerMovementController;
-        private int _layerMask;
+        [SerializeField] private PlayerState playerState;
+        
+        private int _platformLayerMask;
+        private int _obstacleLayerMask;
 
         private void Awake()
         {
-            _layerMask = LayerMask.GetMask("Platform");
+            _platformLayerMask = LayerMask.GetMask("Platform");
+            _obstacleLayerMask = LayerMask.GetMask("Obstacle");
+
         }
 
         private void OnEnable()
         {
             playerMovementController.OnLeap += CheckPlatform;
         }
-        
+
+        private void Update()
+        {
+            CheckObstacle();
+        }
+
         private void CheckPlatform()
         {
-            var platform = Physics2D.OverlapBox(playerMovementController.destination,Vector2.zero,0f, _layerMask);
+            var platform = Physics2D.OverlapBox(playerMovementController.destination,Vector2.zero,0f, _platformLayerMask);
         
             playerMovementController.transform.SetParent(platform != null ? platform.transform : null);
+        }
+
+        private void CheckObstacle()
+        {
+            var obstacle = Physics2D.OverlapBox(playerMovementController.destination,Vector2.zero,0f, _obstacleLayerMask);
+
+            if (obstacle != null)
+                playerState.State = PlayerState.PlayerStates.Dead;
+
         }
         
         private void OnDisable()
---
+++ Assets/Scripts/Player/PlayerState.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace Player
+{
+    public class PlayerState : MonoBehaviour
+    {
+        private PlayerStates _state;
+        [SerializeField] private PlayerAnimator playerAnimator;
+        [SerializeField] private PlayerMovementController playerMovement;
+
+        public enum PlayerStates
+        {
+            Alive,
+            Dead
+        }
+
+        public PlayerStates State
+        {
+            get => _state;
+            set
+            {
+                if (_state == value) return;
+                
+                _state = value;
+                OnStateChange(value);
+            }
+        }
+
+        private void OnStateChange(PlayerStates states)
+        {
+            switch (states)
+            {
+                case PlayerStates.Dead:
+                    playerAnimator.SetSprite(PlayerAnimator.SpriteType.Dead);
+                    playerMovement.StopAllCoroutines();
+                    // Destroy(gameObject);
+                    break;
+            }
+        }
+    }
+}
