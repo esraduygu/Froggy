f27e18c1a96647a61faacff075dce85478cbe68b
Esra Duygu <benesrakaya@gmail.com>
2024-04-01 13:11:40 +0300

    Implemented home script and behaviour.

---
+++ Assets/Scripts/Home.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Home : MonoBehaviour
+{
+    [SerializeField] private GameObject homeFrog;
+
+    private void OnEnable()
+    {
+        homeFrog.SetActive(true);
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+            homeFrog.SetActive(true);
+    }
+
+    private void OnDisable()
+    {
+        homeFrog.SetActive(false);
+    }
+}
